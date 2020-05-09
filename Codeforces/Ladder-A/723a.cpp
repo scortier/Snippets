44093444884afe8a51e5f0e5075ec7f9c285fc62
“your_username” <onlytoaditya@gmail.com>
2020-05-10 00:44:11 +0530

    commit

---
+++ Codeforces/Ladder-A/723a.cpp
@@ -0,0 +1,52 @@
+// QUARANTINE DAYS..;)
+#include <bits/stdc++.h>
+using namespace std;
+#define endl 		    "\n"
+#define test 		    long long int tt;cin>>tt;while(tt--)
+#define lloop(i,a,b)    for(i=a;i<b;i++)
+#define loop(i,b)       for(long long int i=0;i<b;i++)
+#define Loop(i,b)       for(long long int i=1;i<=b;i++)
+#define ll 			    long long int
+#define ld 			    long double
+#define ull 		    unsigned long long int
+#define P               pair<int, int>
+#define pb 			    push_back
+#define mp 			    make_pair
+#define fi 			    first
+#define se 			    second
+#define MOD 		    1000000007
+#define bug 		    cout<<"HY"<<endl
+//#define fast 		    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define resize(A)       A.resize(unique(A.begin(), A.end()) - A.begin())
+#define PI 			    acos(-1.0)
+#define assign(x,val)   memset(x,val,sizeof(x))
+#define prec(val, dig)  fixed << setprecision(dig) << val
+#define vil(vct) 	    vector < ll > vct
+#define vis(vct) 	    vector < string > vct
+#define vip(vct) 	    vector < pair < ll, ll > > vct
+#define pi 			    pair < ll , ll >
+#define pii 			pair < pair < ll, ll >, ll >
+#define Sort(arr) 		sort(arr.begin(), arr.end())
+#define lower(str) 		transform(str.begin(), str.end(), str.begin(), ::tolower);
+#define upper(str) 		transform(str.begin(), str.end(), str.begin(), ::toupper);
+
+void fast() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+/**********====================########################=================***********/
+int32_t main()
+{
+	fast();
+	int a[3];
+	loop(i, 3)
+	{
+		cin >> a[i];
+	}
+	sort(a, a + 3);
+	cout << a[2] - a[0] << endl;
+	return 0;
+}
---
+++ Codeforces/Ladder-A/785a.cpp
@@ -0,0 +1,68 @@
+// QUARANTINE DAYS..;)
+#include <bits/stdc++.h>
+using namespace std;
+#define endl 		    "\n"
+#define test 		    long long int tt;cin>>tt;while(tt--)
+#define lloop(i,a,b)    for(i=a;i<b;i++)
+#define loop(i,b)       for(long long int i=0;i<b;i++)
+#define Loop(i,b)       for(long long int i=1;i<=b;i++)
+#define ll 			    long long int
+#define ld 			    long double
+#define ull 		    unsigned long long int
+#define P               pair<int, int>
+#define pb 			    push_back
+#define mp 			    make_pair
+#define fi 			    first
+#define se 			    second
+#define MOD 		    1000000007
+#define bug 		    cout<<"HY"<<endl
+//#define fast 		    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define resize(A)       A.resize(unique(A.begin(), A.end()) - A.begin())
+#define PI 			    acos(-1.0)
+#define assign(x,val)   memset(x,val,sizeof(x))
+#define prec(val, dig)  fixed << setprecision(dig) << val
+#define vil(vct) 	    vector < ll > vct
+#define vis(vct) 	    vector < string > vct
+#define vip(vct) 	    vector < pair < ll, ll > > vct
+#define pi 			    pair < ll , ll >
+#define pii 			pair < pair < ll, ll >, ll >
+#define Sort(arr) 		sort(arr.begin(), arr.end())
+#define lower(str) 		transform(str.begin(), str.end(), str.begin(), ::tolower);
+#define upper(str) 		transform(str.begin(), str.end(), str.begin(), ::toupper);
+
+void fast() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int32_t main()
+{
+	fast();
+	// string Tetrahedron = '4';
+	// string Cube ='6';
+	// string Octahedron = '8';
+	// string Dodecahedron = '12';
+	// string Icosahedron = '20';
+	map<string, int> m;
+	m["Tetrahedron"]  = 4;
+	m["Cube"]         = 6;
+	m["Octahedron"]   = 8;
+	m["Dodecahedron"] = 12;
+	m["Icosahedron"]  = 20;
+	int n; cin >> n;
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		string s; cin >> s;
+		cnt += m[s];
+	}
+	cout << cnt << endl;
+	return 0;
+}
+
+
+
+
+
