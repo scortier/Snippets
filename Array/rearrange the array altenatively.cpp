734405d66169aee755939b35f23933be353b7a8e
scortier <onlytoaditya@gmail.com>
2020-07-29 01:15:01 +0530

    29-07

---
+++ Array/rearrange the array altenatively.cpp
@@ -0,0 +1,62 @@
+// QUARANTINE DAYS..;)
+#include <bits/stdc++.h>
+using namespace std;
+#define endl 		    "\n"
+#define test 		    int tt;cin>>tt;while(tt--)
+#define fl(i,a,b)       for( int i=a;i<b;i++)
+#define bfl(i,a,b)      for( int i=b-1;i>=a;i--)
+#define ll 			    long long int
+#define pb 			    push_back
+#define mp 			    make_pair
+#define f 			    first
+#define s 			    second
+#define MOD 		    1000000007
+#define PI 			    acos(-1.0)
+#define assign(x,val)   memset(x,val,sizeof(x))
+#define prec(val, dig)  fixed << setprecision(dig) << val
+#define vi   		    vector < int >
+#define pi 			    pair < int , int >
+#define pr(gg)          cout<<gg<<endl
+#define mk(arr,n,type)  type *arr=new type[n];
+const int maxm = 2e6 + 10;
+
+void lage_rho() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+/**********====================########################=================***********/
+
+void re1(int a[], int n)
+{
+	int flag = 1;
+	int temp[n];
+
+
+	fl(i, 0, n)
+	{
+		if (flag)
+			temp[i] = a[l--];
+		else
+			temp[i] = a[s++];
+		flag = !flag;
+	}
+
+	fl(i, 0, n)
+	a[i] = temp[i];
+}
+void solve()
+{
+
+
+}
+
+int32_t main()
+{
+	lage_rho();
+	test
+	solve();
+	return 0;
+}
