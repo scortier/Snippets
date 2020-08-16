0de185582cede26010d78f1edbdfa0aa61ae59a0
scortier <onlytoaditya@gmail.com>
2020-08-16 10:44:54 +0530

    16-8

--- Aug_Challenge_5.cpp
+++ Aug_Challenge_5.cpp
@@ -27,9 +27,73 @@ void lage_rho() {
 /**********=============########################============***********/
 
 
+ll chefWedding(ll arr[], ll n, ll k) {
+	map<ll, ll> mp;
+	ll col[n + 1][n + 1] = {0};
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			col[i][j] = 0;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		mp.clear();
+		for (int j = i; j < n; j++) {
+			col[i][j] = (j == 0) ? 0 : col[i][j - 1];
+			if (mp.count(arr[j])) {
+				if (mp[arr[j]] == 1) {
+					col[i][j]++;
+				}
+				col[i][j]++;
+			}
+			mp[arr[j]]++;
+		}
+	}
+
+	ll ans = 1e18;
+	ll table = 100;
+	ll dp[101][1001] = {0};
+
+	for (int i = 0; i <= table; i++) {
+		for (int j = 0; j <= n; j++) {
+			dp[i][j] = 0;
+		}
+	}
+
+	for (int i = 1; i < n + 1; i++) {
+		dp[1][i] = col[0][i - 1];
+	}
+	for (int i = 1; i <= table; i++) {
+		dp[i][1] = 0;
+	}
+
+	for (int i = 2; i <= table; i++) {
+		for (int j = 2; j <= n; j++) {
+			ll best = 1e18;
+			for (int p = 1; p < j; p++) {
+				best = min(best, dp[i - 1][p] + col[p][j - 1]);
+			}
+			dp[i][j] = best;
+		}
+	}
+
+	for (table = 1; table <= 100; table++) {
+		ans = min(table * k + dp[table][n], ans);
+	}
+
+	return ans;
+}
+
+
 void solve()
 {
+	ll n, k;
+	cin >> n >> k;
+	ll arr[n];
 
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	cout << chefWedding(arr, n, k) << endl;
 
 }
 
